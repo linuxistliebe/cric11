12ff937ce6d37bf5ea749898c3d0ea675e85a726
linuxistliebe <linuxistliebe@gmail.com>
2024-11-07 00:32:13 +0530

    Add files via upload

--- cric11.py
+++ cric11.py
@@ -1,13 +1,14 @@
 import time
-
+toporder=[]
+toporderbat=[]
 
 wlcm='''
-        CricXI - 'You are the Selector'
+        CricXI - 'You are the COACH'
 '''
 
 for wlcmvariable in wlcm:
     print(wlcmvariable,end="")
-    time.sleep(0.049)
+    time.sleep(0.033)
     
 print('''
 ____________
@@ -28,4 +29,27 @@ if initchoice==2:
     while 2<3:
         print("linuxistliebe-",end="")
 
+
+if initchoice==1:
+    print()
+    print("Welcome Mr.Coach")
+    print('''
+[1] TOP ORDER Menu
+[2] MIDDLE ORDER Menu
+[3] LOWER ORDER Menu
+[4] CAPTAINCY Options
+[5] Jackie-Moggy Trophy
+''')
+
+    choice=int(input("Enter Your Choice -> "))
+    if choice==1:
+        print()
+        print("-TOP ORDER-")
+        for i in range(3):
+            nmbtsmn=input("Enter The Name Of Batsman -> ")
+            toporder.append(nmbtsmn.title())
+            
+        print("(@) Top Order Batsman are -",toporder)
         
+
+    
