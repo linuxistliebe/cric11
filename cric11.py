bfbf5bba16d8b74033cab4430128739dac6fc93e
linuxistliebe <linuxistliebe@gmail.com>
2024-11-07 00:11:41 +0530

    Add files via upload

---
+++ cric11.py
@@ -0,0 +1,31 @@
+import time
+
+
+wlcm='''
+        CricXI - 'You are the Selector'
+'''
+
+for wlcmvariable in wlcm:
+    print(wlcmvariable,end="")
+    time.sleep(0.049)
+    
+print('''
+____________
+            |
+[1] START   |
+[2] CREDITS |
+[3] QUIT    |
+____________|
+''')
+
+
+initchoice=int(input("Enter Your Choice -> "))
+
+if initchoice==3:
+    exit()
+    
+if initchoice==2:
+    while 2<3:
+        print("linuxistliebe-",end="")
+
+        
